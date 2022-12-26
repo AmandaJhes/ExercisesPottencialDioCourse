0a5e579754821a8e5c0a00a12e7b939743ae7baa
Amanda Nascimento <87491868+AmandaJhes@users.noreply.github.com>
2022-12-26 09:06:34 -0300

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Operadores de Atribuição");
+
+int a = 10;
+int b = 20;
+int c = a + b;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+Console.Write('\n');
+Console.WriteLine("Combinando Operadores");
+
+Console.WriteLine(a);
+a += 5;
+Console.WriteLine(a);
+a -= 5;
+Console.WriteLine(a);
+a *= 5;
+Console.WriteLine(a);
+a /= 5;
+Console.WriteLine(a);
+
+Console.Write('\n');
+Console.WriteLine("Convertendo tipos de variáveis");
+
+int isNumber = Convert.ToInt32("2");
+Console.WriteLine(isNumber);
+
+int isANumber = int.Parse("3");
+Console.WriteLine(isANumber);
